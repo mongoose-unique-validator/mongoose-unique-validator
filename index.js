c2f3b0b62fd6488e90e57d1aa93609001d9600e7
Blake Haswell <haswell00@gmail.com>
2013-07-09 15:28:24 +1000

    Updated validator to take a mongoose instance rather than a connection, since mongoose MUST have a connection when save is called.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var _ = require('underscore');
 
 module.exports = function (schema, options) {
-    var connection = options.connection;
+    var mongoose = options.mongoose;
     _.each(schema.paths, function (schemaType, path) {
         if (schemaTypeHasUniqueIndex(schemaType)) {
-            var validator = buildUniqueValidator(path, connection);
+            var validator = buildUniqueValidator(path, mongoose);
             schemaType.validate(validator, 'duplicate');
         }
     });
@@ -14,9 +14,9 @@ function schemaTypeHasUniqueIndex(schemaType) {
     return schemaType._index && schemaType._index.unique;
 }
 
-function buildUniqueValidator(path, connection) {
+function buildUniqueValidator(path, mongoose) {
     return function (value, respond) {
-        var model = connection.model(this.constructor.modelName);
+        var model = mongoose.connection.model(this.constructor.modelName);
         var query = buildQuery(path, value);
         var callback = buildValidationCallback(respond);
         model.findOne(query, callback);
