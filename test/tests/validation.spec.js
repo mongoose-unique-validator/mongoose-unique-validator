bcffa23093477091e2a53128e146a0f7439b0772
Mike Botsko <botsko@gmail.com>
2017-09-06 12:31:10 -0700

    Test ValidatorError properties rather than message

--- test/tests/validation.spec.js
+++ test/tests/validation.spec.js
@@ -30,13 +30,15 @@ module.exports = function(mongoose) {
             promise.then(function() {
                 // Try saving a duplicate
                 new User(helpers.USERS[0]).save().catch(function(err) {
-                    expect(err.errors.username.message).to.equal('Error, expected `username` to be unique. Value: `JohnSmith`');
-                    expect(err.errors.username.properties.type).to.equal('unique');
-                    expect(err.errors.username.properties.path).to.equal('username');
+                    expect(err.errors.username.name).to.equal('ValidatorError');
+                    expect(err.errors.username.kind).to.equal('unique');
+                    expect(err.errors.username.path).to.equal('username');
+                    expect(err.errors.username.value).to.equal('JohnSmith');
 
-                    expect(err.errors.email.message).to.equal('Error, expected `email` to be unique. Value: `john.smith@gmail.com`');
-                    expect(err.errors.email.properties.type).to.equal('unique');
-                    expect(err.errors.email.properties.path).to.equal('email');
+                    expect(err.errors.email.name).to.equal('ValidatorError');
+                    expect(err.errors.email.kind).to.equal('unique');
+                    expect(err.errors.email.path).to.equal('email');
+                    expect(err.errors.email.value).to.equal('john.smith@gmail.com');
 
                     done();
                 });
@@ -52,7 +54,10 @@ module.exports = function(mongoose) {
             promise.then(function() {
                 // Try saving a duplicate
                 new User(helpers.USERS[0]).save().catch(function(err) {
-                    expect(err.errors.username.message).to.equal('Error, expected `username` to be unique. Value: `JohnSmith`');
+                    expect(err.errors.username.name).to.equal('ValidatorError');
+                    expect(err.errors.username.kind).to.equal('unique');
+                    expect(err.errors.username.path).to.equal('username');
+                    expect(err.errors.username.value).to.equal('JohnSmith');
 
                     done();
                 });
@@ -142,8 +147,10 @@ module.exports = function(mongoose) {
                 user.save().catch(done).then(function() {
                     user.email = helpers.USERS[0].email;
                     user.save().catch(function(err) {
-                        expect(err).is.not.null;
-                        expect(err.message).to.equal('User validation failed: email: Error, expected `email` to be unique. Value: `john.smith@gmail.com`');
+                        expect(err.errors.email.name).to.equal('ValidatorError');
+                        expect(err.errors.email.kind).to.equal('unique');
+                        expect(err.errors.email.path).to.equal('email');
+                        expect(err.errors.email.value).to.equal('john.smith@gmail.com');
 
                         done();
                     });
@@ -164,8 +171,10 @@ module.exports = function(mongoose) {
                         { email: helpers.USERS[1].email },
                         { runValidators: true, context: 'query' }
                     ).exec().catch(function(err) {
-                        expect(err).is.not.null;
-                        expect(err.message).to.equal('Validation failed: email: Error, expected `email` to be unique. Value: `bob@robertmiller.com`');
+                        expect(err.errors.email.name).to.equal('ValidatorError');
+                        expect(err.errors.email.kind).to.equal('unique');
+                        expect(err.errors.email.path).to.equal('email');
+                        expect(err.errors.email.value).to.equal('bob@robertmiller.com');
 
                         done();
                     });
@@ -190,8 +199,10 @@ module.exports = function(mongoose) {
                         },
                         { runValidators: true, context: 'query' }
                     ).exec().catch(function(err) {
-                        expect(err).is.not.null;
-                        expect(err.message).to.equal('Validation failed: email: Error, expected `email` to be unique. Value: `bob@robertmiller.com`');
+                        expect(err.errors.email.name).to.equal('ValidatorError');
+                        expect(err.errors.email.kind).to.equal('unique');
+                        expect(err.errors.email.path).to.equal('email');
+                        expect(err.errors.email.value).to.equal('bob@robertmiller.com');
 
                         done();
                     });
@@ -211,8 +222,11 @@ module.exports = function(mongoose) {
                     // Try updating this record with an existing email
                     user.email = helpers.USERS[0].email;
                     user.validate().catch(function(err) {
-                        expect(err).to.be.an('object');
-                        expect(err.message).to.equal('User validation failed: email: Error, expected `email` to be unique. Value: `john.smith@gmail.com`');
+                        expect(err.errors.email.name).to.equal('ValidatorError');
+                        expect(err.errors.email.kind).to.equal('unique');
+                        expect(err.errors.email.path).to.equal('email');
+                        expect(err.errors.email.value).to.equal('john.smith@gmail.com');
+
                         done();
                     });
                 });
@@ -227,8 +241,10 @@ module.exports = function(mongoose) {
             promise.then(function() {
                 // Try saving a duplicate
                 new Planet({ _id: 'mercury' }).save().catch(function(err) {
-                    expect(err).is.not.null;
-                    expect(err.message).to.equal('Planet validation failed: _id: Error, expected `_id` to be unique. Value: `mercury`');
+                    expect(err.errors._id.name).to.equal('ValidatorError');
+                    expect(err.errors._id.kind).to.equal('unique');
+                    expect(err.errors._id.path).to.equal('_id');
+                    expect(err.errors._id.value).to.equal('mercury');
 
                     done();
                 });
@@ -274,8 +290,11 @@ module.exports = function(mongoose) {
             promise.then(function() {
                 // Try saving a unique student without a username
                 new Student(helpers.USERS[5]).save().catch(function(err) {
-                    expect(err).is.not.null;
-                    expect(err.message).to.equal('Student validation failed: username: Error, expected `username` to be unique. Value: `JohnSmith`');
+                    expect(err.errors.username.name).to.equal('ValidatorError');
+                    expect(err.errors.username.kind).to.equal('unique');
+                    expect(err.errors.username.path).to.equal('username');
+                    expect(err.errors.username.value).to.equal('JohnSmith');
+
                     done();
                 });
             });
@@ -293,9 +312,10 @@ module.exports = function(mongoose) {
 
                 // Try saving a duplicate
                 user.save().catch(function(err) {
-                    expect(err.errors.email.message).to.equal('Error, expected `email` to be unique. Value: `JOHN.SMITH@GMAIL.COM`');
-                    expect(err.errors.email.properties.type).to.equal('unique');
-                    expect(err.errors.email.properties.path).to.equal('email');
+                    expect(err.errors.email.name).to.equal('ValidatorError');
+                    expect(err.errors.email.kind).to.equal('unique');
+                    expect(err.errors.email.path).to.equal('email');
+                    expect(err.errors.email.value).to.equal('JOHN.SMITH@GMAIL.COM');
 
                     done();
                 });
@@ -323,7 +343,11 @@ module.exports = function(mongoose) {
             promise.then(function() {
                 // Try saving a duplicate
                 new User(otherNestedUser).save().catch(function(err) {
-                    expect(err.errors['contact.email'].message).to.equal('Error, expected `contact.email` to be unique. Value: `john.smith@gmail.com`');
+                    expect(err.errors['contact.email'].name).to.equal('ValidatorError');
+                    expect(err.errors['contact.email'].kind).to.equal('unique');
+                    expect(err.errors['contact.email'].path).to.equal('contact.email');
+                    expect(err.errors['contact.email'].value).to.equal('john.smith@gmail.com');
+
                     done();
                 });
             });
@@ -350,7 +374,11 @@ module.exports = function(mongoose) {
             promise.then(function() {
                 // Try saving a duplicate
                 new User(otherNestedUser).save().catch(function(err) {
-                    expect(err.errors['contact.email'].message).to.equal('Error, expected `contact.email` to be unique. Value: `john.smith@gmail.com`');
+                    expect(err.errors['contact.email'].name).to.equal('ValidatorError');
+                    expect(err.errors['contact.email'].kind).to.equal('unique');
+                    expect(err.errors['contact.email'].path).to.equal('contact.email');
+                    expect(err.errors['contact.email'].value).to.equal('john.smith@gmail.com');
+
                     done();
                 });
             });
@@ -377,7 +405,11 @@ module.exports = function(mongoose) {
             promise.then(function() {
                 // Try saving a duplicate
                 new User(otherNestedUser).save().catch(function(err) {
-                    expect(err.errors['contact.email'].message).to.equal('Error, expected `email` to be unique. Value: `john.smith@gmail.com`');
+                    expect(err.errors['contact.email'].name).to.equal('ValidatorError');
+                    expect(err.errors['contact.email'].kind).to.equal('unique');
+                    expect(err.errors['contact.email'].path).to.equal('email');
+                    expect(err.errors['contact.email'].value).to.equal('john.smith@gmail.com');
+
                     done();
                 });
             });
