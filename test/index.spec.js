ce3e35a53f4400c2188519f4777ceb1af7ba05fe
stenneepro <stenneepro@gmail.com>
2023-04-07 12:00:20 +0300

    Updates to mongoose 7

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
 
 // Connect
 mongoose.connect('mongodb://127.0.0.1:27017/mongoose-unique-validator').catch(err => {
